13baf6256755830a94b964c72ef9d83674eeb996
Edison Sanchez <exs6@cbord.com>
2020-09-13 22:13:54 -0400

    Iniital Version

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,8 +107,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-177898753-1`,
       },
     },
     {
